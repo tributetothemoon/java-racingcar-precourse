d474bebaa1c732603cd8e1cb91b1ca9495abc42e
kkm97351@gmail.com <kkm97351@gmail.com>
2020-12-04 19:09:12 +0900

    feat: 사용자로부터 시도 회수를 묻는 기능 추가
    - 시도 회수를 물어 int로 변환하여 리턴합니다.
    - 숫자가 아니거나, 공백 입력에 대한 예외처리를 View에서 진행합니다.

--- src/main/java/racingcar/View/InputView.java
+++ src/main/java/racingcar/View/InputView.java
@@ -1,19 +1,22 @@
 package racingcar.View;
 
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class InputView {
     private static final String REQUEST_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String REQUEST_TRY_NUMBER_MESSAGE = "시도할 회수는 몇회인가요?";
 
     private static Scanner scanner;
 
-    public static void setScanner(Scanner scanner){
+    public static void setScanner(Scanner scanner) {
         InputView.scanner = scanner;
     }
 
-    public static ArrayList<String> getCarNames(){
+    public static ArrayList<String> getCarNames() {
         ArrayList<String> carNames = new ArrayList<>();
         System.out.println(REQUEST_CAR_NAMES_MESSAGE);
         String rawString = deleteWhiteSpaces(scanner.nextLine());
@@ -28,19 +31,39 @@ public class InputView {
         }
     }
 
-    private static String deleteWhiteSpaces(String string) {
-        return string.replaceAll("\\s+", "");
-    }
-
     private static ArrayList<String> parseString(String rawString) {
         return new ArrayList<>(Arrays.asList(rawString.split(",")));
     }
 
-    private static boolean isEmptyStringOrThrowException(String string){
-        if (string.equals("")){
+    public static int getTryNumber() {
+        System.out.println(REQUEST_TRY_NUMBER_MESSAGE);
+        String rawString = deleteWhiteSpaces(scanner.nextLine());
+        try {
+            isEmptyStringOrThrowException(rawString);
+            return parseIntOrThrowException(rawString);
+        } catch (IllegalArgumentException e) {
+            OutputView.showErrorMessage(e.getMessage());
+            return getTryNumber();
+        }
+    }
+
+    private static String deleteWhiteSpaces(String string) {
+        return string.replaceAll("\\s+", "");
+    }
+
+    private static boolean isEmptyStringOrThrowException(String string) {
+        if (string.equals("")) {
             throw new IllegalArgumentException("입력값이 없습니다.");
         }
         return true;
+    }
+
+    private static int parseIntOrThrowException(String string) {
+        try {
+            return Integer.parseInt(string);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자만 입력해주세요.");
+        }
     }
 
 }
