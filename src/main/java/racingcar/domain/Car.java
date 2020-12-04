2756b0fc12ee76cb2bccf3d87b9777f4338537e5
kkm97351@gmail.com <kkm97351@gmail.com>
2020-12-04 19:20:13 +0900

    feat: Car객체 생성 로직 추가
    - Car를 생성하는 로직createCar(String) 메소드를 추가했습니다.
    - Car를 생성하며 이름의 길이를 검증하는 로직을 추가했습니다.

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,42 @@
+package racingcar.domain;
+
+public class Car {
+    private static final int MAX = 5;
+
+    private final String name;
+    private int position = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    // 추가 기능 구현
+    public static Car createCar(String name){
+        return new Car(name);
+    }
+
+    public static boolean isValidLengthOrThrowException(String string) {
+        if (string.length() > MAX) {
+            throw new IllegalArgumentException("이름의 길이는 " + MAX + "자를 넘어가서는 안 됩니다.");
+        }
+        return true;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || this.getClass() != obj.getClass()) {
+            return false;
+        }
+        Car paramObj = (Car) obj;
+        return this.name.equals(paramObj.name);
+    }
+
+    @Override
+    public int hashCode(){
+        return this.name.hashCode();
+    }
+
+}
