0f826270ad82e28b9f71e7de9c5fdabc852c076c
kkm97351@gmail.com <kkm97351@gmail.com>
2020-12-04 19:39:38 +0900

    feat: Car를 담을 일급 컬렉션 Cars 클래스 생성
    - ArrayList<String>으로 차 이름들을 받아 생성됩니다.
    - 중복에 대한 검증을 실행합니다.

---
+++ src/main/java/racingcar/domain/Cars.java
@@ -0,0 +1,34 @@
+package racingcar.domain;
+
+import javax.management.ListenerNotFoundException;
+import java.util.ArrayList;
+
+public class Cars {
+    private ArrayList<Car> cars;
+
+    private Cars(ArrayList<Car> cars) {
+        this.cars = cars;
+    }
+
+    public static Cars of(ArrayList<String> carNames){
+        ArrayList<Car> cars = new ArrayList<>();
+        carNames.stream()
+                .map(Car::createCar)
+                .filter( i -> isNotContainsOrThrowException(cars, i))
+                .forEach(cars::add);
+        return new Cars(cars);
+    }
+
+    private static boolean isNotContainsOrThrowException(ArrayList<Car> destination, Car car) {
+        if (destination.contains(car)){
+            throw new IllegalArgumentException("자동차 이름이 중복됩니다.");
+        }
+        return true;
+    }
+
+    @Override
+    public String toString(){
+        return this.cars.toString();
+    }
+
+}
