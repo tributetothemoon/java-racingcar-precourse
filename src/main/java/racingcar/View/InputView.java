3ff063b442e7e4e45ce074e40ae5be71712c8cb4
kkm97351@gmail.com <kkm97351@gmail.com>
2020-12-04 18:16:07 +0900

    feat: 입력을 담당할 InputView 클래스 생성
    - 입력에서 사용될 Scanner객체를 생성하는 메소드 setScanner(Scanner)와 같이 구현되었습니다.

---
+++ src/main/java/racingcar/View/InputView.java
@@ -0,0 +1,12 @@
+package racingcar.View;
+
+import java.util.Scanner;
+
+public class InputView {
+    private static Scanner scanner;
+
+    public static void setScanner(Scanner scanner){
+        InputView.scanner = scanner;
+    }
+
+}
