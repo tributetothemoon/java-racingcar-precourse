ea8819b5f9d0cde91e7c7aa790aef8061e271f98
kkm97351@gmail.com <kkm97351@gmail.com>
2020-12-04 20:24:13 +0900

    feat: Application에서 RacingCarGame 실행 로직 구현
    - Application에서 유저로부터 차 이름과 시도 회수 입력을 받아 컨트롤러 객체를 생성하고 실행하는 로직을 구현했습니다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,6 +1,9 @@
 package racingcar;
 
+import racingcar.controller.RaicingCarGame;
+import racingcar.domain.Cars;
 import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 import java.util.Scanner;
 
@@ -9,5 +12,25 @@ public class Application {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
         InputView.setScanner(scanner);
+
+        Cars cars = getCarNamesFromUser();
+        int tryNumber = getTryNumberFromUser();
+        RaicingCarGame raicingCarGame = new RaicingCarGame(cars, tryNumber);
+        raicingCarGame.play();
+
+    }
+
+    private static Cars getCarNamesFromUser(){
+        try {
+            return Cars.of(InputView.getCarNames());
+        } catch (IllegalArgumentException e) {
+            OutputView.showErrorMessage(e.getMessage());
+            return getCarNamesFromUser();
+        }
+    }
+
+    private static int getTryNumberFromUser() {
+        return InputView.getTryNumber();
     }
 }
+
