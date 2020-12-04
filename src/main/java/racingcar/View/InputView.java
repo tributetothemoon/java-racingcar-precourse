0dba2e412cd3c7622f85ec0df4a49664e8ac89b8
kkm97351@gmail.com <kkm97351@gmail.com>
2020-12-04 18:47:35 +0900

    feat: 자동차 이름 입력 기능 추가. 예외 메세지 출력 기능 추가
    - 입력값을 쉼표로 분리해 ArrayList<String>으로 리턴합니다.
    - 공백 입력에 대한 예외 처리를 진행했습니다.
    - 예외 시 메세지를 출력할 기능이 필요해 OutputView에 구현했습니다.

--- src/main/java/racingcar/View/InputView.java
+++ src/main/java/racingcar/View/InputView.java
@@ -1,12 +1,46 @@
 package racingcar.View;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class InputView {
+    private static final String REQUEST_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+
     private static Scanner scanner;
 
     public static void setScanner(Scanner scanner){
         InputView.scanner = scanner;
+    }
+
+    public static ArrayList<String> getCarNames(){
+        ArrayList<String> carNames = new ArrayList<>();
+        System.out.println(REQUEST_CAR_NAMES_MESSAGE);
+        String rawString = deleteWhiteSpaces(scanner.nextLine());
+
+        try {
+            isEmptyStringOrThrowException(rawString);
+            carNames = parseString(rawString);
+            return carNames;
+        } catch (IllegalArgumentException e) {
+            OutputView.showErrorMessage(e.getMessage());
+            return getCarNames();
+        }
+    }
+
+    private static String deleteWhiteSpaces(String string) {
+        return string.replaceAll("\\s+", "");
+    }
+
+    private static ArrayList<String> parseString(String rawString) {
+        return new ArrayList<>(Arrays.asList(rawString.split(",")));
+    }
+
+    private static boolean isEmptyStringOrThrowException(String string){
+        if (string.equals("")){
+            throw new IllegalArgumentException("입력값이 없습니다.");
+        }
+        return true;
     }
 
 }
--- src/main/java/racingcar/View/OutputView.java
+++ src/main/java/racingcar/View/OutputView.java
@@ -1,5 +1,10 @@
 package racingcar.View;
 
 public class OutputView {
-    private static final String ErrorPrefix = "[ERROR]";
+    private static final String ErrorPrefix = "[ERROR] ";
+
+    public static void showErrorMessage(String message){
+        System.out.println(ErrorPrefix + message);
+    }
+
 }
